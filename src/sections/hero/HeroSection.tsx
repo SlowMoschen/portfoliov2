6ef7f9d204b4b4d21c3a1ac319621bedaf2af91f
phil@home <philipp.millner@outlook.com>
2024-05-05 01:30:37 +0200

    chore: Update HeroSection layout and content

--- src/sections/hero/HeroSection.tsx
+++ src/sections/hero/HeroSection.tsx
@@ -1,12 +1,21 @@
 import Background from "./Background";
-import './styles/HeroSection.css'
+import "./styles/HeroSection.css";
+import svg from "../../assets/hero.svg";
 
 export default function HeroSection() {
-    return (
-        <>
+  return (
+    <>
+      <section>
         <Background />
-        <h1>Willkommen</h1>
-        </>
-
-    )
+        <div>
+          <h1 className="animated-text">Willkommen</h1>
+          <p>
+            Ich bin ein Fullstack-Entwickler, der sich auf die Entwicklung von Webanwendungen
+            spezialisiert hat.
+          </p>
+        </div>
+        <img src={svg} alt="hero" />
+      </section>
+    </>
+  );
 }
