a92115e9803b8ebf0c26db494a2dee7ffa91a452
phil@home <philipp.millner@outlook.com>
2024-05-06 21:41:46 +0200

    chore: Update Navbar background color and add mobile navigation
    The background color of the Navbar has been updated to the primary color. Additionally, a mobile navigation menu has been added for screens with a width less than 768 pixels.

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -4,16 +4,20 @@ import "./styles/Navbar.css";
 import linkedin from "../../assets/icons/linkedin.svg";
 import github from "../../assets/icons/github.svg";
 import { useEffect, useState } from "react";
+import { useScreenSize } from "../../utils/hooks/useScreenSize";
 
 export default function Navbar() {
   const sections = ["home", "projekte", "skills", "kontakt"];
   const [active, setActive] = useState<string>("home");
   const [canvasBottom, setCanvasBottom] = useState<number>(0);
-  const [background, setBackground] = useState<"transparent" | "secondary">("transparent");
+  const [background, setBackground] = useState<"transparent" | "primary">("transparent");
+  const [isNavVisible, setIsNavVisible] = useState<boolean>(false);
+  const { screenWidth } = useScreenSize();
+  const isMobile = screenWidth < 768;
 
   const changeBackground = (scrollPosition: number) => {
     if (scrollPosition > canvasBottom) {
-      setBackground("secondary");
+      setBackground("primary");
     } else {
       setBackground("transparent");
     }
@@ -33,28 +37,74 @@ export default function Navbar() {
   });
 
   useEffect(() => {
+    if (!isMobile) setIsNavVisible(true);
+
     const canvasHeight = document.querySelector("#canvas")?.getBoundingClientRect();
     setCanvasBottom(canvasHeight?.bottom || 0);
 
     window.addEventListener("scroll", handleScroll);
     sections.forEach((section) => {
-        const element = document.querySelector(`#${section}`);
-        element && observer.observe(element);
-        });
+      const element = document.querySelector(`#${section}`);
+      element && observer.observe(element);
+    });
 
     return () => {
-        window.removeEventListener("scroll", handleScroll);
-        sections.forEach((section) => {
-            const element = document.querySelector(`#${section}`);
-            element && observer.unobserve(element);
-        });
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+      window.removeEventListener("scroll", handleScroll);
+      sections.forEach((section) => {
+        const element = document.querySelector(`#${section}`);
+        element && observer.unobserve(element);
+      });
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const navBarStyles = {
-    backgroundColor: background === "transparent" ? "transparent" : "var(--text)",
+    backgroundColor: isMobile ? "var(--primary)" : background === "transparent" ? "transparent" : "var(--primary)",
   };
+
+  if (isMobile) {
+    return (
+      <>
+        <nav className="mobile-nav" style={navBarStyles}>
+        <span
+          className="material-symbols-outlined burger"
+          onClick={() => setIsNavVisible(!isNavVisible)}
+        >
+          menu
+        </span>
+        {
+          isNavVisible && (
+            <div className="mobile-nav-content">
+              {sections.map((section) => (
+                <Button
+                  key={section}
+                  onClick={() => {
+                    setIsNavVisible(false);
+                    setActive(section);
+                    jumpTo(section);
+                  }}
+                  size="lg"
+                  styles={active === section ? "accent" : "secondary"}
+                  variant="text"
+                >
+                  {section}
+                </Button>
+              ))}
+              <div>
+                <a href="https://www.linkedin.com/in/philipp-millner-4167b5286/" target="_blank">
+                  <img src={linkedin} alt="" className="link-img" />
+                </a>
+                <a href="https://github.com/SlowMoschen" target="_blank">
+                  <img src={github} alt="" className="link-img github-icon" />
+                </a>
+              </div>
+            </div>
+          )
+        }
+        </nav>
+      </>
+    );
+  }
 
   return (
     <nav className="navbar" style={navBarStyles}>
@@ -69,6 +119,7 @@ export default function Navbar() {
             size="lg"
             styles={active === section ? "accent" : "secondary"}
             variant="text"
+            style={{ fontWeight: 800, letterSpacing: "0.1rem"}}
           >
             {section}
           </Button>
